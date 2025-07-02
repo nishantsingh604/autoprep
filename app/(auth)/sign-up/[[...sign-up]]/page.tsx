0179870e81168e233bacd86988b436e62b2d8e70
nishantsingh604 <nishantrohilla28@gmail.com>
2025-07-02 23:40:33 +0530

    Authentication with clerk done, need to solve a bug, and will do the database integration next

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,13 +1,12 @@
 // "use client";
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 import React from "react";
-
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <SignIn />
+      <SignUp />
     </div>
-  
-);
+  );
 }
+    
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+// "use client";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div><UserButton />
       <h1 className="text-4xl font-bold text-center mt-10">
         Welcome to AutoPrep
       </h1>
